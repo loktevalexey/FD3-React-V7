9ce2cbf82685d82eb904b4f9eea439c9aaee5507
loktev <loktev@tut.by>
2022-07-26 20:05:36 +0300

    useReducer

--- H_hooks/Hooks78_useReducer/components/Anketa.js
+++ H_hooks/Hooks78_useReducer/components/Anketa.js
@@ -1,8 +1,22 @@
 ﻿import React, { useReducer } from 'react';
 
-import { personReducer } from "./reducer";
-
 import "./Anketa.css";
+
+function personReducer(state, action) {
+  switch (action.type) {
+    case 'age1':
+      return {...state, age: state.age+1};
+    case 'ageN':
+      return {...state, age: state.age+action.val};
+    case 'genderM':
+      return {...state, gender: "М"};
+    case 'genderF':
+      return {...state, gender: "F"};
+    default:
+      //return state;
+      throw new Error('unknown action');
+  }
+}
 
 export default props => {
 
