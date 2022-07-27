81f814a191e2485d2c756b6eedbe9c8aaa41214c
loktev <loktev@tut.by>
2022-07-27 18:16:13 +0300

    fragment

---
+++ H_hooks/Fragment65/src/components/Counter.js
@@ -0,0 +1,24 @@
+﻿import React, { useState, useEffect, Fragment } from 'react';
+
+import './Counter.css'
+
+export default props => {
+
+  const [cnt, setCnt] = useState(0);
+
+  useEffect(
+    ()=>{
+      const timer=setInterval(()=>setCnt(prevCnt=>prevCnt+1),1000);
+      console.log("таймер запущен");
+    },
+    []
+  );
+
+  return (
+    <div>
+      <span>Счётчик: {cnt}</span><br/>
+      <span>Счётчик: {cnt}</span><br/>
+      <span>Счётчик: {cnt}</span>
+    </div>
+  );
+};
