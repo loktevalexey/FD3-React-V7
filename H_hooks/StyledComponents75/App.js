e33c994b9e1773883d267a6fbca05db6195ad6af
loktev <loktev@tut.by>
2022-07-27 16:51:19 +0300

    styled

---
+++ H_hooks/StyledComponents75/App.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from './components/Container';
+
+ReactDOM.render(
+  <Container/>
+  , document.getElementById('container') 
+);
+
---
+++ H_hooks/StyledComponents75/components/Container.js
@@ -0,0 +1,31 @@
+﻿import React, { useState } from 'react';
+import styled, { css } from 'styled-components'
+
+const BorderedDiv = styled.div`
+  border: solid red 2px;
+  border-radius: 5px;
+  padding: 20px;
+  width: 150px;
+`;
+
+const BackgroundedDiv = styled.div`
+  border-radius: 5px;
+  ${ attrs => css`
+    background: ${attrs.ccc==="eng"?"red":"green"};
+  `}
+`;
+
+export default props => {
+
+  const [lang, setLang] = useState('eng');
+
+  return (
+    <BorderedDiv>
+      <BackgroundedDiv ccc={lang}>
+        Язык: {lang}<br/>
+        <button onClick={ () => setLang('eng') }>английский</button>
+        <button onClick={ () => setLang('rus') }>русский</button>
+      </BackgroundedDiv>
+    </BorderedDiv>
+  );
+};
