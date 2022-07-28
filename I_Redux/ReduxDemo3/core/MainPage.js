ca58876ee93dd779b44d31437c2e16d5e794947f
loktev <loktev@tut.by>
2022-07-28 14:25:31 +0300

    redux hooks

---
+++ I_Redux/ReduxDemo3/core/MainPage.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import combinedReducer from '../redux/reducers.js';
+import { CounterButton } from '../components/CounterButton/CounterButton';
+
+const store=createStore(combinedReducer);
+
+export const MainPage = () => (
+      <Provider store={store}>
+          <div>
+              <h1>демо работы Redux</h1>
+              <CounterButton counterid={111} />
+              <CounterButton counterid={222} />
+          </div>
+      </Provider>
+);
