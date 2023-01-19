623756c056012ae92edb7fc9c08e61866ca39e0f
loktev <loktev@tut.by>
2023-01-19 21:06:10 +0300

    RH32 React.memo

---
+++ H_hooks/Hooks32_React_memo/components/MobileClient.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+import "./MobileClient.css";
+
+const MobileClient = ({fio,balance}) => {
+
+  console.log("render MobileClient "+fio);
+
+  return (
+    <div className='MobileClient'>
+      <span className='MobileClientBalance'>{balance}</span>
+      <span className='MobileClientFIO'>{fio}</span>
+    </div>
+  );
+};
+
+export default React.memo(MobileClient);
+// если пропсы не изменились - React.memo возвращает результат предыдущего рендера
